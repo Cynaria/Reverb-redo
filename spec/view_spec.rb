8ec63193f45fe0c4b46366d15cacc0a2d79eab76
Cynaria <Elizabeth.J.Cruz@gmail.com>
2015-01-07 10:32:39 -0500

    View: #parse_options

---
+++ spec/view_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper.rb"
+
+describe View do
+	let(:view) {View.new}
+	describe '#parse_options' do
+		context 'ARGV options' do
+			it 'parses combine with files' do
+				expect(view).to receive(:combine).with(["./data/space.txt"])
+				view.parse_options(["combine", "./data/space.txt"])
+			end
+			it 'parses last_name' do
+				expect(view).to receive(:get_last_names)
+				view.parse_options(["last_name"])
+			end
+
+			it 'parses birthdate' do
+				expect(view).to receive(:get_birthdate)
+				view.parse_options(["birthdate"])
+			end
+
+			it 'parses gender' do
+				expect(view).to receive(:get_gender)
+				view.parse_options(["gender"])
+			end
+		end
+	end
+
+	describe '#combine' do
+		it 'calls PersonController#combine_files' do
+			expect(PersonController).to receive(:combine_files).with("./data/space.txt")
+			view.combine("./data/space.txt")
+		end
+	end
+
+
+
+
+end
---
+++ view.rb
@@ -0,0 +1,28 @@
+require_relative "person_controller.rb"
+class View
+	def parse_options(opt)
+		if opt[0] == "combine"
+			combine(opt[1..-1  ])
+		elsif opt[0] == "last_name"
+			get_last_names
+		elsif opt[0] == "birthdate"
+			get_birthdate
+		elsif opt[0] == "gender"
+			get_gender
+		end
+
+	end
+
+	def combine(files)
+		PersonController.combine_files(files)
+	end
+
+	def get_last_names
+	end
+
+	def get_birthdate
+	end
+
+	def get_gender
+	end
+end
